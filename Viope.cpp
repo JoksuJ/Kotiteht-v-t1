7d446b705d57261a7b8ed92e063e9ee02da44efc
Joakim Jokipii <joakim.jokipii@gmail.com>
2020-09-03 10:58:47 +0300

    Add project files.

---
+++ Viope.cpp
@@ -0,0 +1,65 @@
+/*
+
+//KOTITEHTÄVÄ 1
+
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "Syota luku: ";
+	int ika;
+	cin >> ika;
+	cout << "Syotit luvun " << ika << endl;
+
+	return 0;
+}
+// KOTITEHTÄVÄ 2
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "Ollakko vai eiko olla." << "\n" << endl;
+	cout << "Kas siina pulma." << endl;
+
+	return 0;
+
+}
+
+
+// KOTITEHTÄVÄ 3
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Anna ikasi: ";
+	int ika;
+	cin >> ika;
+	cout << "50 vuoden kuluttua olet " << ika + 50 << " vuotias" << endl;
+
+	return 0;
+}
+
+*/
+
+//KOTITEHTÄVÄ 4
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Syota 1. luku: ";
+	int luku1;
+	cin >> luku1;
+
+	cout << "Syota 2. luku: ";
+	int luku2;
+	cin >> luku2;
+
+	cout << "Lukujen erotus on " << luku1 - luku2 << endl;
+	cout << "Lukujen tulo on " << luku1 * luku2 << endl;
+
+	return 0;
+}
